fec131d8fd558f53eb12cbe15e76336e9f832ddc
bFraley <bFraley@users.noreply.github.com>
2016-09-14 10:41:42 -0400

    install requests module

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
+import sys
+
 from app import app
+try:
+	import settings
+except ImportError:
+    print("Please copy settings-dist.py to settings.py and update your AP key")
+    sys.exit()
 
 app.run(debug=True)
